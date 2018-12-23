12bb1bc92bcf8c898a4dca5cdff21a9184de20d1
LambdaXymox <lambda.xymox@gmail.com>
2018-12-23 15:02:02 -0600

    Remove kinematix parameters from camera model.

--- src/camera.rs
+++ src/camera.rs
@@ -5,7 +5,7 @@ use std::fmt;
 
 
 #[derive(Clone, Debug)]
-pub struct Camera {
+pub struct PerspectiveCamera {
     // Camera parameters.
     pub near: f32,
     pub far: f32,
@@ -13,8 +13,6 @@ pub struct Camera {
     pub aspect: f32,
 
     // Camera kinematics.
-    pub speed: f32,
-    pub yaw_speed: f32,
     pub pos: Vector3,
     pub fwd: Vector4,
     pub rgt: Vector4,
@@ -28,30 +26,29 @@ pub struct Camera {
     pub view_mat: Matrix4,
 }
 
-impl Camera {
+impl PerspectiveCamera {
     pub fn new(
         near: f32, far: f32, fov: f32, aspect: f32,
-        cam_speed: f32, cam_yaw_speed: f32, cam_pos: Vector3,
-        fwd: Vector4, rgt: Vector4, up: Vector4, axis: Quaternion) -> Camera {
+        cam_pos: Vector3,
+        fwd: Vector4, rgt: Vector4, up: Vector4, axis: Vector3) -> PerspectiveCamera {
 
         let proj_mat = math::perspective((fov, aspect, near, far));
         let trans_mat = Matrix4::from_translation(cam_pos);
-        let rot_mat = Matrix4::from(axis);
+        let axis_quat = Quaternion::from_sv(0.0, axis);
+        let rot_mat = Matrix4::from(axis_quat);
         let view_mat = rot_mat * trans_mat;
 
-        Camera {
+        PerspectiveCamera {
             near: near,
             far: far,
             fov: fov,
             aspect: aspect,
 
-            speed: cam_speed,
-            yaw_speed: cam_yaw_speed,
             pos: cam_pos,
             fwd: fwd,
             rgt: rgt,
             up: up,
-            axis: axis,
+            axis: axis_quat,
 
             proj_mat: proj_mat,
             trans_mat: trans_mat,
@@ -61,14 +58,12 @@ impl Camera {
     }
 }
 
-impl fmt::Display for Camera {
+impl fmt::Display for PerspectiveCamera {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         writeln!(f, "Camera Model:").unwrap();
         writeln!(f, "near: {}", self.near).unwrap();
         writeln!(f, "far: {}", self.far).unwrap();
         writeln!(f, "aspect: {}", self.aspect).unwrap();
-        writeln!(f, "speed: {}", self.speed).unwrap();
-        writeln!(f, "yaw_speed: {}", self.yaw_speed).unwrap();
         writeln!(f, "pos: {}", self.pos).unwrap();
         writeln!(f, "fwd: {}", self.fwd).unwrap();
         writeln!(f, "rgt: {}", self.rgt).unwrap();
