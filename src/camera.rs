cb0d08e0d82026e97408b4528e3120d685e3070e
LambdaXymox <lambda.xymox@gmail.com>
2018-12-24 17:39:01 -0600

    Add orthographic camera.

--- src/camera.rs
+++ src/camera.rs
@@ -155,3 +155,82 @@ impl fmt::Display for PerspectiveCamera {
         writeln!(f, "view_mat: {}", self.view_mat)
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct OrthographicCamera {
+    // Camera frustum parameters.
+    pub left: f32,
+    pub right: f32,
+    pub bottom: f32,
+    pub top: f32,
+    pub near: f32,
+    pub far: f32,
+
+    // Camera kinematics.
+    pub origin: Vector3,
+    pub fwd: Vector4,
+    pub rgt: Vector4,
+    pub up: Vector4,
+    pub axis: Quaternion,
+
+    // Camera matrices.
+    pub proj_mat: Matrix4,
+    pub trans_mat: Matrix4,
+    pub rot_mat: Matrix4,
+    pub view_mat: Matrix4,
+}
+
+impl OrthographicCamera {
+    pub fn new(
+        near: f32, far: f32, bottom: f32, top: f32, left: f32, right: f32,
+        cam_pos: Vector3,
+        fwd: Vector4, rgt: Vector4, up: Vector4, axis: Vector3) -> OrthographicCamera {
+
+        let proj_mat = math::frustum((left, right, bottom, top, near, far));
+        let trans_mat = Matrix4::from_translation(cam_pos);
+        let axis_quat = Quaternion::from_sv(0.0, axis);
+        let rot_mat = Matrix4::from(axis_quat);
+        let view_mat = rot_mat * trans_mat;
+
+        OrthographicCamera {
+            left: left,
+            right: right,
+            bottom: bottom,
+            top: top,
+            near: near,
+            far: far,
+
+            origin: cam_pos,
+            fwd: fwd,
+            rgt: rgt,
+            up: up,
+            axis: axis_quat,
+
+            proj_mat: proj_mat,
+            trans_mat: trans_mat,
+            rot_mat: rot_mat,
+            view_mat: view_mat,
+        }
+    }
+}
+
+impl fmt::Display for OrthographicCamera {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "Camera Model:").unwrap();
+        writeln!(f, "left: {}", self.left).unwrap();
+        writeln!(f, "right: {}", self.right).unwrap();
+        writeln!(f, "bottom: {}", self.bottom).unwrap();
+        writeln!(f, "top: {}", self.top).unwrap();
+        writeln!(f, "near: {}", self.near).unwrap();
+        writeln!(f, "far: {}", self.far).unwrap();
+        writeln!(f, "origin: {}", self.origin).unwrap();
+        writeln!(f, "fwd: {}", self.fwd).unwrap();
+        writeln!(f, "rgt: {}", self.rgt).unwrap();
+        writeln!(f, "up: {}", self.up).unwrap();
+        writeln!(f, "axis: {}", self.axis).unwrap();
+        writeln!(f, "proj_mat: {}", self.proj_mat).unwrap();
+        writeln!(f, "trans_mat: {}", self.trans_mat).unwrap();
+        writeln!(f, "rot_mat: {}", self.rot_mat).unwrap();
+        writeln!(f, "view_mat: {}", self.view_mat)
+    }
+}
