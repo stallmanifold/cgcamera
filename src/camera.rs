035515a823727535e9ac2e073f4f13427067b85b
LambdaXymox <lambda.xymox@gmail.com>
2018-12-26 18:49:50 -0600

    Add frustum bounds tests for perspective camera.

--- src/camera.rs
+++ src/camera.rs
@@ -429,9 +429,35 @@ mod orthographic_camera_tests {
 
 #[cfg(test)]
 mod perspective_camera_tests {
+    use cgmath::Vector3;
     use super::{
         CameraAttitude, Frustum, FrustumFov, PerspectiveCamera, PerspectiveFovCamera
     };
+
+    struct AxisAlignedBoundingBox {
+        left: f32,
+        right: f32,
+        bottom: f32,
+        top: f32,
+        near: f32,
+        far: f32,
+    }
+
+    impl AxisAlignedBoundingBox {
+        fn new(left: f32, right: f32, bottom: f32, top: f32, near: f32, far: f32) -> Self {
+            Self { left, right, bottom, top, near, far }
+        }
+
+        fn contains(&self, point: Vector3) -> bool {
+            (point.x >= self.left) && (point.x <= self.right) &&
+                (point.y >= self.bottom) && (point.y <= self.top) &&
+                (point.z >= self.far && point.z <= self.near)
+        }
+
+        fn unit_aabb() -> Self {
+            Self::new(-1.0, 1.0, -1.0, 1.0, 1.0, -1.0)
+        }
+    }
 
 
     #[test]
@@ -456,5 +482,59 @@ mod perspective_camera_tests {
 
         assert_eq!(persp_box.view_mat, persp_fov.view_mat);
         assert_eq!(persp_box.proj_mat, persp_fov.proj_mat);
+    }
+
+    #[test]
+    fn test_perspective_camera_should_map_points_inside_frustum_inside_canonical_view_volume() {
+        let left = -4.0;
+        let right = 4.0;
+        let bottom = -4.0;
+        let top = 4.0;
+        let near = 0.1;
+        let far = 4.0;
+        let frustum = Frustum::new(left, right, bottom, top, near, far);
+
+        let origin = cgmath::vec3((0.0, 0.0, 5.0));
+        let forward = cgmath::vec4((0.0, 0.0, -1.0, 0.0));
+        let right = cgmath::vec4((1.0, 0.0, 0.0, 0.0));
+        let up = cgmath::vec4((0.0, 1.0, 0.0, 0.0));
+        let rotation_axis = cgmath::vec3((0.0, 0.0, -1.0));
+        let attitude = CameraAttitude::new(origin, forward, right, up, rotation_axis);
+        let camera = PerspectiveCamera::new(frustum, attitude);
+
+        let p_wor = cgmath::vec4((-3.0, 3.0, 3.0, 1.0));
+        let p_cam = camera.view_mat * p_wor;
+        let p_cvv = camera.proj_mat * p_cam;
+        let p_cvv = cgmath::vec3((p_cvv.x, p_cvv.y, p_cvv.z));
+        let cvv = AxisAlignedBoundingBox::unit_aabb();
+
+        assert!(cvv.contains(p_cvv), "p_wor = {}; p_cam = {}; p_cvv = {}", p_wor, p_cam, p_cvv);
+    }
+
+    #[test]
+    fn test_orthographic_camera_should_map_points_outside_frustum_outside_canonical_view_volume() {
+        let left = -4.0;
+        let right = 4.0;
+        let bottom = -4.0;
+        let top = 4.0;
+        let near = 0.1;
+        let far = 4.0;
+        let frustum = Frustum::new(left, right, bottom, top, near, far);
+
+        let origin = cgmath::vec3((0.0, 0.0, 5.0));
+        let forward = cgmath::vec4((0.0, 0.0, -1.0, 0.0));
+        let right = cgmath::vec4((1.0, 0.0, 0.0, 0.0));
+        let up = cgmath::vec4((0.0, 1.0, 0.0, 0.0));
+        let rotation_axis = cgmath::vec3((0.0, 0.0, -1.0));
+        let attitude = CameraAttitude::new(origin, forward, right, up, rotation_axis);
+        let camera = PerspectiveCamera::new(frustum, attitude);
+
+        let p_wor = cgmath::vec4((300.0, 300.0, 300.0, 1.0));
+        let p_cam = camera.view_mat * p_wor;
+        let p_cvv = camera.proj_mat * p_cam;
+        let p_cvv = cgmath::vec3((p_cvv.x, p_cvv.y, p_cvv.z));
+        let cvv = AxisAlignedBoundingBox::unit_aabb();
+
+        assert!(!cvv.contains(p_cvv), "p_wor = {}; p_cam = {}; p_cvv = {}", p_wor, p_cam, p_cvv);
     }
 }
