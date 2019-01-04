9f79f53fa82d36ed515c1bc934905815275f76c1
LambdaXymox <lambda.xymox@gmail.com>
2019-01-04 02:57:28 -0600

    Add tests for camera facing the origin on the x-axis.

--- tests/test_orthographic_camera.rs
+++ tests/test_orthographic_camera.rs
@@ -43,10 +43,10 @@ fn test_unit_viewing_volume_projection_matrix_should_be_mirroring_matrix() {
 fn test_orthographic_camera_mirror_mat_trans_mat() {
     let camera = orthographic_mirror_matrix_camera_model();
     let trans_mat = Matrix4::new(
-        1.0, 0.0,  0.0, 0.0,
-        0.0, 1.0,  0.0, 0.0,
-        0.0, 0.0,  1.0, 0.0,
-        0.0, 0.0, -5.0, 1.0
+        1.0, 0.0,   0.0, 0.0,
+        0.0, 1.0,   0.0, 0.0,
+        0.0, 0.0,   1.0, 0.0,
+        0.0, 0.0,  -5.0, 1.0
     );
 
     assert_eq!(camera.trans_mat, trans_mat);
@@ -72,7 +72,7 @@ fn test_orthographic_camera_mirror_mat_view_mat() {
         1.0, 0.0,  0.0,  0.0,
         0.0, 1.0,  0.0,  0.0,
         0.0, 0.0,  1.0,  0.0,
-        0.0, 0.0, -5.0,  1.0
+        0.0, 0.0,  5.0,  1.0
     );
 
     assert_eq!(camera.view_mat, view_mat);
@@ -102,10 +102,10 @@ fn orthographic_camera_z_axis() -> OrthographicCamera {
 fn test_orthographic_camera_z_axis_trans_mat() {
     let camera = orthographic_camera_z_axis();
     let trans_mat = Matrix4::new(
-        1.0, 0.0,  0.0, 0.0,
-        0.0, 1.0,  0.0, 0.0,
-        0.0, 0.0,  1.0, 0.0,
-        0.0, 0.0, -5.0, 1.0
+        1.0, 0.0,   0.0, 0.0,
+        0.0, 1.0,   0.0, 0.0,
+        0.0, 0.0,   1.0, 0.0,
+        0.0, 0.0,  -5.0, 1.0
     );
 
     assert_eq!(camera.trans_mat, trans_mat);
@@ -131,7 +131,7 @@ fn test_orthographic_camera_z_axis_view_mat() {
         1.0, 0.0,  0.0,  0.0,
         0.0, 1.0,  0.0,  0.0,
         0.0, 0.0,  1.0,  0.0,
-        0.0, 0.0, -5.0,  1.0
+        0.0, 0.0,  5.0,  1.0
     );
 
     assert_eq!(camera.view_mat, view_mat);
@@ -142,9 +142,81 @@ fn test_orthographic_camera_z_axis_proj_mat() {
     let camera = orthographic_camera_z_axis();
     let proj_mat = Matrix4::new(
         0.25,  0.0,   0.0, 0.0,
-        0.0, 0.25,   0.0, 0.0,
-        0.0,  0.0, -0.25, 0.0,
-        0.0,  0.0,   0.0, 1.0
+        0.0,  0.25,   0.0, 0.0,
+        0.0,   0.0, -0.25, 0.0,
+        0.0,   0.0,   0.0, 1.0
+    );
+
+    assert_eq!(camera.proj_mat, proj_mat);
+}
+
+fn orthographic_camera_x_axis() -> OrthographicCamera {
+    let left = -4.0;
+    let right = 4.0;
+    let bottom = -4.0;
+    let top = 4.0;
+    let near = 4.0;
+    let far = -4.0;
+    let frustum = Frustum::new(left, right, bottom, top, near, far);
+
+    let origin = cgmath::vec3((5.0, 0.0, 0.0));
+    let forward = cgmath::vec4((1.0, 0.0, 0.0, 0.0));
+    let rgt = cgmath::vec4((1.0, 0.0, 0.0, 0.0));
+    let up = cgmath::vec4((0.0, 1.0, 0.0, 0.0));
+    let rotation_axis = cgmath::vec3((0.0, 0.0, -1.0));
+    let attitude = CameraAttitude::new(origin, forward, rgt, up, rotation_axis);
+    let camera = OrthographicCamera::new(frustum, attitude);
+
+    camera
+}
+
+#[test]
+fn test_orthographic_camera_x_axis_trans_mat() {
+    let camera = orthographic_camera_x_axis();
+    let trans_mat = Matrix4::new(
+         1.0, 0.0,  0.0, 0.0,
+         0.0, 1.0,  0.0, 0.0,
+         0.0, 0.0,  1.0, 0.0,
+        -5.0, 0.0,  0.0, 1.0
+    );
+
+    assert_eq!(camera.trans_mat, trans_mat);
+}
+
+#[test]
+fn test_orthographic_camera_x_axis_rot_mat() {
+    let camera = orthographic_camera_x_axis();
+    let rot_mat = Matrix4::new(
+        0.0, 0.0, 1.0, 0.0,
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    );
+
+    assert_eq!(camera.rot_mat, rot_mat);
+}
+
+#[test]
+fn test_orthographic_camera_x_axis_view_mat() {
+    let camera = orthographic_camera_x_axis();
+    let view_mat = Matrix4::new(
+        1.0, 0.0,  0.0,  0.0,
+        0.0, 1.0,  0.0,  0.0,
+        0.0, 0.0,  1.0,  0.0,
+        0.0, 0.0,  5.0,  1.0
+    );
+
+    assert_eq!(camera.view_mat, view_mat);
+}
+
+#[test]
+fn test_orthographic_camera_x_axis_proj_mat() {
+    let camera = orthographic_camera_x_axis();
+    let proj_mat = Matrix4::new(
+        0.25,  0.0,   0.0, 0.0,
+        0.0,  0.25,   0.0, 0.0,
+        0.0,   0.0, -0.25, 0.0,
+        0.0,   0.0,   0.0, 1.0
     );
 
     assert_eq!(camera.proj_mat, proj_mat);
