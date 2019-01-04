4ddc55ff1a66f74f70c8c05579ec5730368d0305
LambdaXymox <lambda.xymox@gmail.com>
2019-01-04 17:09:56 -0600

    Add tests for y-axis aligned perspective camera.

--- tests/test_perspective_camera.rs
+++ tests/test_perspective_camera.rs
@@ -147,3 +147,75 @@ fn test_perspective_camera_x_axis_proj_mat() {
 
     assert_eq!(camera.proj_mat, proj_mat);
 }
+
+fn perspective_camera_y_axis() -> PerspectiveCamera {
+    let left = -4.0;
+    let right = 4.0;
+    let bottom = -4.0;
+    let top = 4.0;
+    let near = 4.0;
+    let far = -4.0;
+    let frustum = Frustum::new(left, right, bottom, top, near, far);
+
+    let origin = cgmath::vec3((0.0, 5.0, 0.0));
+    let forward = cgmath::vec4((0.0, 1.0, 0.0, 0.0));
+    let rgt = cgmath::vec4((-1.0, 0.0, 0.0, 0.0));
+    let up = cgmath::vec4((0.0, 0.0, 1.0, 0.0));
+    let rotation_axis = cgmath::vec3((0.0, -1.0, 0.0));
+    let attitude = CameraAttitude::new(origin, forward, rgt, up, rotation_axis);
+    let camera = PerspectiveCamera::new(frustum, attitude);
+
+    camera
+}
+
+#[test]
+fn test_perspective_camera_y_axis_trans_mat() {
+    let camera = perspective_camera_y_axis();
+    let trans_mat = Matrix4::new(
+        1.0,  0.0, 0.0, 0.0,
+        0.0,  1.0, 0.0, 0.0,
+        0.0,  0.0, 1.0, 0.0,
+        0.0, -5.0, 0.0, 1.0
+    );
+
+    assert_eq!(camera.trans_mat, trans_mat);
+}
+
+#[test]
+fn test_perspective_camera_y_axis_rot_mat() {
+    let camera = perspective_camera_y_axis();
+    let rot_mat = Matrix4::new(
+        -1.0, 0.0,  0.0, 0.0,
+        0.0, 1.0,  0.0, 0.0,
+        0.0, 0.0, -1.0, 0.0,
+        0.0, 0.0,  0.0, 1.0
+    );
+
+    assert_eq!(camera.rot_mat, rot_mat);
+}
+
+#[test]
+fn test_perspective_camera_y_axis_view_mat() {
+    let camera = perspective_camera_y_axis();
+    let view_mat = Matrix4::new(
+        -1.0,  0.0,  0.0,  0.0,
+        0.0,  1.0,  0.0,  0.0,
+        0.0,  0.0, -1.0,  0.0,
+        0.0, -5.0,  0.0,  1.0
+    );
+
+    assert_eq!(camera.view_mat, view_mat);
+}
+
+#[test]
+fn test_perspective_camera_y_axis_proj_mat() {
+    let camera = perspective_camera_y_axis();
+    let proj_mat = Matrix4::new(
+        0.25,  0.0,   0.0, 0.0,
+        0.0,  0.25,   0.0, 0.0,
+        0.0,   0.0, -0.25, 0.0,
+        0.0,   0.0,   0.0, 1.0
+    );
+
+    assert_eq!(camera.proj_mat, proj_mat);
+}
